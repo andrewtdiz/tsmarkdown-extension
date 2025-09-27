915fadf04cd17533bf85e34173490fca67fee239
Andrew DiZenzo <andrewdizenzojhu@gmail.com>
2025-09-27 10:06:24 -0700

    1.0.9

--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,11 @@
 import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
 
-import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient/node';
-
-let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	const serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-
-	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-		}
-	};
-
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
-
-	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
-
-	client.start();
-	console.log("Started local extension!");
+	console.log("Started TSMD extension!");
 }
 
-export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+export function deactivate() {
+	console.log("Stopped TSMD extension!");
 }
